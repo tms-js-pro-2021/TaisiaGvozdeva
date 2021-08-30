c7e23d72654954b8feb4b2258e2be8491f581bec
TaisEyyy <tsukishimataisey@gmail.com>
2021-08-30 18:32:12 +0300

    >todoAppIsReady

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import { useLocation } from "react-router-dom"
+import Button from "./Button"
+
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
+
+  return(
+    <header className = 'header'>
+      <h1>{title}</h1>
+      {location.pathname === '/' && <Button color={showAdd ? 'black' : 'green'} text={showAdd ? 'Close' : 'Add Task'} onClick={onAdd}/>}
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker'
+}
+export default Header
+
